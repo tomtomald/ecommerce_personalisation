f70f415d7d9fa1e45ad0592785ecda1dd826c17d
Leonard Kinzinger <leo.kinzinger@t-online.de>
2024-05-15 19:41:24 +0200

    Update import path for customised AI design and peer-inspired design

--- webflow/src/index.js
+++ webflow/src/index.js
@@ -2,12 +2,14 @@
 function initApp() {
   //displayLoader();
   const path = window.location.pathname;
-
-  console.log("Hello World!");
   
   // Handle authentication and user display for app routes
   if (path.startsWith('/generate-products') || path.startsWith('/find-products')) {
     import('../src/generate_products.js').then(module => module.render())
+  }
+
+  else if (path.startsWith('/customised-ai-design') || path.startsWith('/peer-inspired-design')) {
+    import('../src/generate_products_updated.js').then(module => module.render())
   }
 }
 
