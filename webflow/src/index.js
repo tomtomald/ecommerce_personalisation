618f5fbacd89e96c01c395d748600f0f016add27
tomtomald <64801681+tomtomald@users.noreply.github.com>
2024-06-06 17:23:17 +0200

    additional landing pages

--- webflow/src/index.js
+++ webflow/src/index.js
@@ -8,7 +8,7 @@ function initApp() {
     import('../src/generate_products.js').then(module => module.render())
   }
 
-  else if (path.startsWith('/customised-ai-design') || path.startsWith('/peer-inspired-design')|| path.startsWith('/pure-recommendation')) {
+  else if (path.startsWith('/customised-ai-design') || path.startsWith('/peer-inspired-design') || path.startsWith('/sequential-process-ai') || path.startsWith('/sequential-process-pure') || path.startsWith('/pure-recommendation')) {
     import('../src/generate_products_updated.js').then(module => module.render())
   }
 }
